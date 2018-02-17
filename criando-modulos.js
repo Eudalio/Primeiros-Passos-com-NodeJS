aeee9ae85c7a12fafa8df87481cf479d83995a90
eudalio <tonnydallios@gmail.com>
2018-02-16 21:03:09 -0300

    Criando meus próprios módulos

---
+++ criando-modulos.js
@@ -0,0 +1,8 @@
+let modulo1 = require('./modulos/modulo1');
+let modulo2 = require('./modulos/modulo2');
+
+modulo1('Eudálio Sousa');
+
+modulo2.minhaIdade(21);
+modulo2.minhaProfissao('Estudante');
+modulo2.minhaCidade('Quixeramobim');
